f6a09e6a5fb4c3e2113c3e284e2a8a4ae50b18d7
jumabekovs <so.fly.azash@gmail.com>
2020-11-05 02:59:42 +0600

    Added view for customer and creating customers

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logOutUser, name='logout'),
+
     path('', views.home, name='home'),
+    path('user/', views.userPage, name='user-page'),
     path('products/', views.products, name='products'),
     path('customer/<str:pk>/', views.customer, name='customer'),
-    path('create/', views.createOrder, name='create_order'),
+
+    path('create/<str:pk>', views.createOrder, name='create_order'),
     path('update/<str:pk>/', views.updateOrder, name='update_order'),
     path('delete/<str:pk>', views.deleteOrder, name='delete_order'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,65 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group     # allows to choose a group
+
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
+from .decorators import unauthenticated_user, allowed_users, admin_only
 
 
+@unauthenticated_user
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='Customer')
+            user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+            )
+
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+@unauthenticated_user
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is Incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logOutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')  # first that will see user is to login
+@admin_only    # allows only admins, customers will see only particular info
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -18,34 +74,65 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['Customer'])
+def userPage(request):
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    print('Orders:', orders)
+
+    context = {'orders': orders, 'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending}
+    return render(request, 'accounts/user.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['Admin'])
 def products(request):
     products = Product.objects.all()
 
     return render(request, 'accounts/products.html', {'products': products})
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['Admin'])  # this decorator does not permit others to see, except Admin group
 def customer(request, pk):
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
     orders_count = orders.count()
 
-    context = {'customer': customer, 'orders': orders, 'orders_count': orders_count}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {'customer': customer, 'orders': orders, 'orders_count': orders_count, 'myFilter': myFilter}
     return render(request, 'accounts/customer.html', context)
 
 
-def createOrder(request):
-
-    form = OrderForm()   # We used it when creating order form
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['Admin'])
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    # form = OrderForm(initial={'customer': customer})   #this will fill automatically the user
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)     # We used it when creating order form
     if request.method == "POST":
         # print('Printing POST:', request.POST) to check what we are sending
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer) # just added request.POST
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
-    context = {'form': form}
+
+    context = {'formset': formset}  #changed from form to formset
     return render(request, 'accounts/order_form.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['Admin'])
 def updateOrder(request, pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -59,6 +146,8 @@ def updateOrder(request, pk):
     return render(request, 'accounts/order_form.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['Admin'])
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == "POST":
